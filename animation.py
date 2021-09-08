cb7db268029d1e9bd818271eab3fc591a7a96160
A.H. Kadar <31695244+nae9on@users.noreply.github.com>
2021-09-08 22:18:52 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -3,7 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-ReferenceTime: np.datetime64 = np.datetime64('2021-08-18T00:00:00.000000000')
+precision = 's'
+ReferenceTime = np.datetime64('2021-08-18T00:00:00.000000000')
 
 
 class XlsParser:
@@ -26,15 +27,21 @@ class XlsParser:
 
         # Get the time delta
         dt = (self.time - ReferenceTime)
-        dt = dt / 1e+6  # convert ns to ms
-        self.time_delta = dt + self.millisecond
-        self.time_delta = self.time_delta.astype(int)
+
+        if precision == 's':
+            dt = dt.astype('timedelta64[s]')  # convert ns to s
+            self.time_delta = dt
+        else:
+            dt = dt.astype('timedelta64[ms]')  # convert ns to ms
+            self.time_delta = dt + self.millisecond
+
+        self.time_delta = self.time_delta.astype(np.int64)
 
         self.sampled_timeline = np.arange(0, self.time_delta[-1] + 1, 1)
-        self.sampled_status = np.zeros((self.sampled_timeline.size,), dtype=np.int)
+        self.sampled_status = np.zeros((self.sampled_timeline.size,), dtype=np.int64)
         self.sampled_status[0: self.time_delta[0]] = -1
         self.sampled_status[self.time_delta] = self.status
-        for itr in range(0, self.time_delta.size - 2, 1):
+        for itr in range(0, self.time_delta.size - 1, 1):
             self.sampled_status[self.time_delta[itr] + 1: self.time_delta[itr + 1]] = self.status[itr]
 
 
@@ -70,50 +77,59 @@ if __name__ == "__main__":
     window_length_minutes = 30
     time_shift_minutes = 1
 
-    window_length_ms = int(window_length_minutes * 60 * 1000)
-    time_shift_ms = int(time_shift_minutes * 60 * 1000)
+    if precision == 's':
+        window_length = int(window_length_minutes * 60)
+        time_shift = int(time_shift_minutes * 60)
+    else:
+        window_length = int(window_length_minutes * 60 * 1000)
+        time_shift = int(time_shift_minutes * 60 * 1000)
 
     fig = plt.figure(figsize=(20, 4))
-    ax = plt.axes(xlim=(0, window_length_ms), ylim=(-8, 6))
-    ax.set_xlabel('Time window in ms')
+    ax = plt.axes(xlim=(0, window_length), ylim=(-8, 6))
+    ax.set_xlabel('Time window in ' + precision)
     ax.set_ylabel('Zone status')
     ref, = ax.plot([], [], lw=1, color='k')
     result, = ax.plot([], [], lw=1, color='b')
-    false_positives, = ax.plot([], [], lw=1, color='r')
-    false_negatives, = ax.plot([], [], lw=1, color='y')
+    yres_FP, = ax.plot([], [], lw=1, color='r')
+    yres_FN, = ax.plot([], [], lw=1, color='y')
     label = ax.text(2, 4, '', ha='left', va='center', fontsize=10, color="Black")
-    ax.legend([ref, result, false_positives, false_negatives], ['Loop', 'TrafiCamAI', 'FP', 'FN'])
+    ax.legend([ref, result, yres_FP, yres_FN], ['Loop', 'TrafiCamAI', 'FP', 'FN'])
 
     # initialization function: plot the background of each frame
     def init():
         ref.set_data([], [])
         result.set_data([], [])
-        false_positives.set_data([], [])
-        false_negatives.set_data([], [])
+        yres_FP.set_data([], [])
+        yres_FN.set_data([], [])
         label.set_text('')
-        return ref, result, false_positives, false_negatives, label
+        return ref, result, yres_FP, yres_FN, label
+
+    false_positives = np.maximum(Result.sampled_status[0: max_index] - Reference.sampled_status[0: max_index], 0)
+    false_negatives = np.minimum(Result.sampled_status[0: max_index] - Reference.sampled_status[0: max_index], 0)
 
     def animate(i):
-        t_begin = int(i*time_shift_ms)
-        t_end = min(int(i*time_shift_ms+window_length_ms), Reference.sampled_status.size, Result.sampled_status.size)
+        t_begin = int(i * time_shift)
+        t_end = min(int(i * time_shift + window_length), max_index-1)
         print(t_begin, t_end)
-        xref = np.arange(0, window_length_ms)
+
         yref = Reference.sampled_status[t_begin:t_end]
         yres = Result.sampled_status[t_begin:t_end] - 2
-        yres_false_positives = np.maximum(Result.sampled_status[t_begin:t_end]-Reference.sampled_status[t_begin:t_end], 0 * xref) - 4
-        yres_false_negatives = np.minimum(Result.sampled_status[t_begin:t_end]-Reference.sampled_status[t_begin:t_end], 0 * xref) - 6
+        y_FP = false_positives[t_begin:t_end] - 4
+        y_FN = false_negatives[t_begin:t_end] - 6
+
+        xref = np.arange(0, yref.size)
 
         ref.set_data(xref, yref)
         result.set_data(xref, yres)
-        false_positives.set_data(xref, yres_false_positives)
-        false_negatives.set_data(xref, yres_false_negatives)
-        label.set_text('Time range in minutes = '+str(t_begin/60000)+str(' - ')+str(t_end/60000))
-        return ref, result, false_positives, false_negatives, label
+        yres_FP.set_data(xref, y_FP)
+        yres_FN.set_data(xref, y_FN)
+        label.set_text('Time range = '+str(t_begin)+str(' - ')+str(t_end))
+        return ref, result, yres_FP, yres_FN, label
 
     plot_animation = True
 
     if plot_animation:
-        anim = animation.FuncAnimation(fig=fig, func=animate, frames=int(Reference.sampled_timeline.size/time_shift_ms),
+        anim = animation.FuncAnimation(fig=fig, func=animate, frames=int(Reference.sampled_timeline.size / time_shift),
                                        init_func=init, interval=200, blit=True)
         anim.save('./timeline.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
         # plt.show()
